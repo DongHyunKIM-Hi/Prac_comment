4d8fb7b4f52a01ce401b530182a4cf3c8cf56695
Viva <happybird00@naver.com>
2021-01-09 20:17:02 +0900

    준비

--- src/store/modules/comments.js
+++ src/store/modules/comments.js
@@ -1,17 +1,13 @@
-import * as commentsApi from '../../api/comments';
+import * as commentsApi from "../../api/comments";
 
+const add_commit = "ADD";
+const delete_commit = "DEL";
 
-export const initialState = {
-    
-};
+export const initialState = {};
 
-
-export default function comments ( state = initialState , action ){
-    switch (action.type){
-        
-        default : 
-            return state
-
-    }
-
+export default function comments(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
