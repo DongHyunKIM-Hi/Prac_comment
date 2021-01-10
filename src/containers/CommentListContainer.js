30cdff4e2fc0cdd4bfcdab590cc4e2cf612540cb
happybird00@naver.com <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-01-10 16:43:34 +0900

    프로젝트 환경 세팅

--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import CommentList from '../components/CommentList';
 
 
 function CommentListContainer(){
-
+    const state = useSelector(state=>state.)
     return (
       <CommentList />
     )
--- src/store/modules/comments.js
+++ src/store/modules/comments.js
@@ -1,12 +1,28 @@
 import * as commentsApi from "../../api/comments";
 
-const add_commit = "ADD";
-const delete_commit = "DEL";
+const ADD_COMMENTS = "ADD_COMMENTS";
+const DELETE_COMMENTS = "DELETE_COMMENTS";
 
-export const initialState = {};
+export const add_comment = (detail) => ({ type: ADD_COMMENTS, detail });
+export const delete_comment = (id) => ({ type: DELETE_COMMENTS, id });
+
+export const initialState = [
+  {
+    id: 1,
+    title: "연습임돠",
+  },
+  {
+    id: 2,
+    title: "연습",
+  },
+];
 
 export default function comments(state = initialState, action) {
   switch (action.type) {
+    case ADD_COMMENTS:
+      return state.concat(action.detail);
+    case DELETE_COMMENTS:
+      return state.filter((comment) => comment.id !== action.id);
     default:
       return state;
   }
